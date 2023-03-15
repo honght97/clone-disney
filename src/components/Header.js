1cfc43bf800e5d5c78e9823ccd7e6fcb35445079
Hoang Hong <honghoang@bincg.com>
2023-03-15 09:09:25 +0700

    next

---
+++ src/components/Header.js
@@ -0,0 +1,55 @@
+import React from "react";
+import styled from "styled-components";
+
+const Header = (props) => {
+  return (
+    <Nav>
+      <Logo>
+        <img src="/images/logo.svg" alt="Disney+" />
+      </Logo>
+      <NavMenu>menu</NavMenu>
+    </Nav>
+  );
+};
+const Nav = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 70px;
+  background-color: #090b13;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 36px;
+  letter-spacing: 16px;
+  z-index: 3;
+`;
+const Logo = styled.a`
+  padding: 0;
+  width: 80px;
+  margin-top: 4px;
+  max-height: 70px;
+  font-size: 0;
+  display: inline-block;
+  img {
+    width: 100%;
+    display: block;
+  }
+`;
+const NavMenu = styled.div`
+  align-items: center;
+  display: flex;
+  flex: row nowrap;
+  height: 100%;
+  justify-content: flex-end;
+  margin-top: 0;
+  margin-bottom: 0;
+  padding: 0;
+  margin-right: auto;
+  margin-left: 25px;
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+export default Header;
